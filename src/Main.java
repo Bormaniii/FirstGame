a9c77b1f199ec125da0d299bff05ba918bfb87ed
Kamil Borecki <borecki.kamil@gmail.com>
2020-04-01 21:01:49 +0200

    Model and Service packages added

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Model.api.AbstractEntity;
+import Model.impl.Hero;
+import Service.impl.HeroService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HeroService heroService = new HeroService();
+        Hero hero = new Hero();
+
+        heroService.setStats(hero);
+        heroService.showStats(hero);
+
+        Fight fight = new Fight();
+        fight.easy();
+
+    }
+}
